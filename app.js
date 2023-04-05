f2ca9e8942730b4b639f0d564bf9b73b1522a447
haqqibrahim <enessyibrahim@gmail.com>
2023-04-05 10:05:52 +0100

    cors edit

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 3000;
-const studentAuthRoutes = require('./Routes/Student/StudentAuthRoutes');
-const vendorAuthRoutes = require('./Routes/Vendor/VendorAuthRoutes');
+const studentAuthRoutes = require("./Routes/Student/StudentAuthRoutes");
+const vendorAuthRoutes = require("./Routes/Vendor/VendorAuthRoutes");
 const studentTransactionRoutes = require("./Routes/Student/StudentTransactionRoutes");
 const vendorTransactionRoutes = require("./Routes/Vendor/VendorTransactionRoutes");
-const adminAuthRoutes = require('./Routes/Admin/AdminAuthRoutes');
+const adminAuthRoutes = require("./Routes/Admin/AdminAuthRoutes");
 
 const app = express();
 
@@ -23,25 +23,21 @@ mongoose
 // parse requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({
-  origin: 'http://localhost:8081',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 app.use(express.json());
 const allowCORS = (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 };
 // Use the authentication routes
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Payvry" });
 });
-app.use("/student/api",allowCORS, studentAuthRoutes)
-app.use("/student/api",allowCORS, studentTransactionRoutes)
-app.use("/vendor/api",allowCORS,vendorAuthRoutes)
-app.use("/vendor/api",allowCORS,vendorTransactionRoutes)
-app.use("/admin/api",allowCORS,adminAuthRoutes)
+app.use("/student/api", allowCORS, studentAuthRoutes);
+app.use("/student/api", allowCORS, studentTransactionRoutes);
+app.use("/vendor/api", allowCORS, vendorAuthRoutes);
+app.use("/vendor/api", allowCORS, vendorTransactionRoutes);
+app.use("/admin/api", allowCORS, adminAuthRoutes);
 // start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
