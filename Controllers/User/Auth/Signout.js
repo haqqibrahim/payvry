767405dcfd910762197525afd8ba7d9d9d907bef
haqqibrahim <enessyibrahim@gmail.com>
2023-05-20 23:23:52 +0100

    New files

--- Controllers/User/Auth/Signout.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-
-exports.signout = async (req, res) => {
-  res.cookie("jwt", "", { maxAge: 1 }); // deleting the token from the cookies
-  res.status(200).json({ message: "User logged out successfully" });
-};
-
-
