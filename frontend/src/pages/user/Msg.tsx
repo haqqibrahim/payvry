c42a9c90b435e297cbcfd624985d03095aa78210
haqqibrahim <enessyibrahim@gmail.com>
2023-06-10 10:02:49 +0100

    Message page

--- frontend/src/pages/user/Msg.tsx
+++ frontend/src/pages/user/Msg.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-
+import { BsWhatsapp } from "react-icons/bs"
 const Msg = () => {
   return (
-    <div className="bg-white m-auto flex items-center justify-center">
-    <p
-      className="font-semibold text-[20px] leading-[26px] text-center pt-44 text-green"
-    >
-      Account Created Succesfully, you can now return to WhatsApp.
-    </p>
-  </div>
+    <div className="bg-white m-auto  flex flex-col items-center justify-center h-screen w-screen" >
+      <p onClick={() => window.open("https://wa.me/2349057516507")}
+        className="font-semibold text-[20px] leading-[26px] text-center text-green cursor-pointer"
+      >
+        Seems like we good here now, click to <br /> now return to WhatsApp 😌.
+      </p>
+      <BsWhatsapp className='cursor-pointer' style={{ color: "green", width: "10%", height: "10%", paddingTop: "10px" }} onClick={() => window.open("https://wa.me/2349057516507")} />
+
+    </div>
   )
 }
 
